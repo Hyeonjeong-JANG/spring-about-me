b5a50e131775f63a64759805772280f4c6021a63
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-24 18:04:26 +0900

    하다가 집에 감

--- src/main/java/com/example/aboutme/AboutMeApplication.java
+++ src/main/java/com/example/aboutme/AboutMeApplication.java
@@ -1,12 +1,8 @@
 package com.example.aboutme;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AboutMeApplication {
@@ -19,20 +15,5 @@ public class AboutMeApplication {
         System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
 
         SpringApplication.run(AboutMeApplication.class, args);
-    }
-
-    @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector(Http11NioProtocol.class.getName());
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        return connector;
     }
 }
--- src/main/java/com/example/aboutme/_core/config/WebSecurityConfig.java
+++ src/main/java/com/example/aboutme/_core/config/WebSecurityConfig.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +21,26 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/webrtc/**").authenticated() // 화상 채팅 관련 URL에 대해서만 인증 강제
+                        .requestMatchers("/webrtc/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .requiresChannel(channel -> channel
-                        .requestMatchers("/webrtc/**").requiresSecure() // HTTPS 강제
-                );
+                        .requestMatchers("/webrtc/**").requiresSecure()
+                )
+                .cors(withDefaults()); // CORS 설정 추가
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://about-me.free.nf:8443"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
